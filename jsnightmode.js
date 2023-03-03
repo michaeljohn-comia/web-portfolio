bd6d72926115f442ad6cbcc630599d0ce1344585
Michael John Comia <michaelcomia1999@gmail.com>
2023-03-03 16:04:36 +0800

    MOD: web-portfolio-michael.html to index.html

--- jsnightmode.js
+++ jsnightmode.js
@@ -2,10 +2,10 @@ var design = document.querySelector(".design");
 var nightmode_btn = document.querySelector("#nightmode");
 var lightmode_btn = document.querySelector("#lightmode");
 
-nightmode_btn.addEventListener("click",function(){
-    design.setAttribute("href","web-portofolio2.css");
+nightmode_btn.addEventListener("click", function () {
+    design.setAttribute("href", "web-portofolio2.css");
 })
 
-lightmode_btn.addEventListener("click",function(){
-    design.setAttribute("href","web-portfolio.css");
+lightmode_btn.addEventListener("click", function () {
+    design.setAttribute("href", "web-portfolio.css");
 })
