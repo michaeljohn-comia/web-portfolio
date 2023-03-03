51203c380bead697229079cf1d3fef76bf762b47
zeus garcia <zgarcia@integranet.ph>
2023-03-03 15:52:09 +0800

    Initial Commit

---
+++ jsnightmode.js
@@ -0,0 +1,11 @@
+var design = document.querySelector(".design");
+var nightmode_btn = document.querySelector("#nightmode");
+var lightmode_btn = document.querySelector("#lightmode");
+
+nightmode_btn.addEventListener("click",function(){
+    design.setAttribute("href","web-portofolio2.css");
+})
+
+lightmode_btn.addEventListener("click",function(){
+    design.setAttribute("href","web-portfolio.css");
+})
